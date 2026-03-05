f01787d8d32fb5d27c0d60166e8685dfa6d7e66a
Elizabeth Tulloch <elizabethtulloch11@gmail.com>
2026-03-04 22:14:37 -0500

    yeild code added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 2026Phase1
 learning decorators, async, and Python SME Topics
+Decorators: 
+Yeild: pauses the generator method , holds on to its place there, doesnt tear down like normal methods. will stay there til next yeild is called. 
+     - safer to use for loops so you know it wont go over 
--- decorators.py
+++ decorators.py
@@ -1,24 +1,35 @@
 # decortor - a fucntion that extencds that behvior of another function 
-from contextlib import contextmanager
+import functools
+
+def adding_each_scoop(limit): 
+    msg = "added scoop"
+    while limit != 0: 
+        yield msg 
+        limit-=1
+    print("scoops added")
+
 
 #decorator
 def add_sprinkles(func): 
+    @functools.wraps(func) # so you dont lose the idenity of the og method 
     def wrapper(*args, **kwargs): # need to have this so that it doesnt get run anytime the decortor is applied 
         print("you add sprinkles")
-        func(*args, **kwargs)
+        return func(*args, **kwargs) # actualy returns the funcs return value
     return wrapper
 
 def add_fudge(func):
     def wrapper(*args, **kwargs):  
         print("adding fudge")
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper
 
 @add_fudge 
 @add_sprinkles
 def get_icecream(flavor):
     print("hello")
-
+    generator = adding_each_scoop(3)
+    for scoop in generator: 
+        print(scoop)
     print(f"heres your {flavor} icecream")
 
 get_icecream("strawbery")
